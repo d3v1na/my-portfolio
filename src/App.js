b19b8891ab365438df59e6c0cb30a77c683d6a0e
Devina Bhatnagar <db133@snu.edu.in>
2023-04-03 23:11:39 +0530

    modified:   package.json 	modified:   src/App.css 	modified:   src/App.js new file:   src/me.png 	new file:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import{
+  useScroll,
+  useTransform,
+  motion
+} from "framer-motion";
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const x = useTransform(scrollYProgress, [0, 1], [0, -600]);
+  const x1 = useTransform(scrollYProgress, [0, 1], [0, 600]);
+  const x2 = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  const x3 = useTransform(scrollYProgress, [0, 1], [0, 100]);
   return (
     <body>
     <title>Devina Bhatnagar</title>
@@ -14,7 +24,18 @@ function App() {
     </nav>
     <h1 className="cool">Devina Bhatnagar</h1>
     <h2>nsfouk HVukxkj</h2>
-    <h3>i'm a designer i also coded this website</h3>
+    <h3>i'm a designer i also made this website</h3>
+    <motion.h4 style={{x: x}}>me me me me me me me me me me me me</motion.h4>
+    <motion.h4 style={{x: x1}}>me me me me me me me me me me me me</motion.h4>
+    <img src={require('./me.png')}/>
+    <motion.h4 style={{x: x2}}>about about about about about about</motion.h4>
+    <motion.h4 style={{x: x3}}>about about about about about about</motion.h4>
+    <h6>i’m a graphics and ui/ux Designer, currently working as design lead for niteouts.
+‍
+i'm pursuing a b.tech in computer science from shiv nadar university.
+
+please scroll down to view some of the work i've done.</h6>
+
     </body>
   );
 }
